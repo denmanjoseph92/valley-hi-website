87f181eb15be11341e3a85c5b10a61511c9579e3
JoseDe2 <JoseDe2@vsp.com>
2017-04-28 16:56:18 -0700

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,15 +14,69 @@ export class AppComponent {
   }
 
   public info = {
-    emailAddress: 'valleyhicc@gmail.com',
-    phoneNumber: '1 800 000 0000',
+    emailAddress: 'valleyhicc@valleyhicc.com',
+    phoneNumber: '(916) 684 2120',
     companyName: 'Valley Hi Country Club',
     companyDescription: 'Description for valley hi'
   };
 
-  public mainNavLinks : {name:string,path:string}[] = [
-    {name:'Home',path:'/home'},
-    {name:'Services',path:'/services'},
-    {name:'Contact Us',path:'/contact'}
+  public mainNavLinks : {name:string,path:string,subpaths:any[]}[] = [
+    {name:'History', path:'/history', subpaths:[]},
+    {
+      name:'Membership',
+      path:'/membership',
+      subpaths:[
+        {name:'Benefits Of Membership', path:'/benefits'},
+        {name:'Types Of Membership', path:'/types'},
+        {name:'Gold Card Program', path:'/gold-card'},
+        {name:'Club Fees', path:'/fees'},
+        {name:'Pool', path:'/pool'}
+      ]
+    },
+    {
+      name:'Golf',
+      path:'/golf',
+      subpaths:[
+        {name:'Course Tour', path:'/course-tour'},
+        {name:'Course Update', path:'/course-update'},
+        {name:'Scorecard', path:'/scorecard'},
+        {name:'Pro Shop', path:'/pro-shop'},
+        {name:'Practice Facilities', path:'/practice-facilities'},
+        {name:'Tournaments', path:'/tournaments'}
+      ]
+    },
+    {
+      name:'Weddings',
+      path:'/weddings',
+      subpaths:[
+        {name:'Fairway Package', path:'/fairway-package'},
+        {name:'Lakeside Package', path:'/lakeside-package'},
+        {name:'Wedding Album', path:'/album'}
+      ]
+    },
+    {
+      name:'Experience',
+      path:'/experience',
+      subpaths:[
+        {name:'Course Aerial Tour', path:'/aerial-tour'},
+        {name:'Clubhouse', path:'/clubhouse'},
+        {name:'Dining', path:'/dining'}
+      ]
+    },
+    {
+      name:'Events',
+      path:'/events',
+      subpaths:[
+        {name:'Future Events', path:'/future-events'},
+        {name:'Ballroom Information', path:'/ballroom-info'}
+      ]
+    },
+    {
+      name:'Contact Us',
+      path:'/contact-us',
+      subpaths:[
+        {name:'Map & Directions', path:'/map'}
+      ]
+    }
   ];
 }
